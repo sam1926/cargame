8f3834b1d6b3e6603fc945501991e98d30561308
sam1926 <62940328+sam1926@users.noreply.github.com>
2022-12-26 12:20:22 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+session_destroy();
+
+header("Location: GAME1.HTML");
+exit;
+
+?>
---
+++ sign_in.php
@@ -0,0 +1,35 @@
+<?php
+
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = sprintf("SELECT * FROM user
+                    WHERE email = '%s'",
+                   $mysqli->real_escape_string($_POST["email"]));
+    
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+    
+    if ($user) {
+        
+        if (password_verify($_POST["password"], $user["password_hash"])) {
+            
+            session_start();
+            
+            session_regenerate_id();
+            
+            $_SESSION["user_id"] = $user["id"];
+            
+            header("Location: index2.php");
+            exit;
+        }
+    }
+    
+    $is_invalid = true;
+}
+
+?>
